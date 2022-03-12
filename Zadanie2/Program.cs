e925a0ffa9bb7e6259782e1525a9453c204ec928
Mateusz Kurowski <kurowskimateusz04@gmail.com>
2022-03-12 13:46:30 +0100

    Zadanie 3

--- Zadanie2/Program.cs
+++ Zadanie2/Program.cs
@@ -2,24 +2,23 @@
 
 using ver2;
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     internal static class Program
     {
         static void Main(string[] args)
         {
-            var xerox = new Copier();
+            var xerox = new MultifunctionalDevice();
             xerox.PowerOn();
             IDocument doc1 = new PDFDocument("aaa.pdf");
-            xerox.Print(in doc1);
+            xerox.SendFax(in doc1);
 
-            IDocument doc2;
-            xerox.Scan(out doc2);
+            xerox.DownloadFax();
 
-            xerox.ScanAndPrint();
+            xerox.Fax();
             System.Console.WriteLine(xerox.Counter);
-            System.Console.WriteLine(xerox.PrintCounter);
-            System.Console.WriteLine(xerox.ScanCounter);
+            System.Console.WriteLine(xerox.FaxCounter);
+            System.Console.WriteLine(xerox.DownloadFaxCounter);
         }
     }
 }
---
+++ Zadanie3/Scanner.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ver3
+{
+    public class Scanner : BaseDevice, IScanner
+    {
+        public int ScanCounter { get; set; }
+
+        public void Print(in IDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(out IDocument document)
+        {
+            document = null;
+            if (state == IDevice.State.off)
+                return;
+            ScanCounter++;
+            Console.WriteLine($"{DateTime.Now} Scan: {ScanCounter}");
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType)
+        {
+            document = null;
+            if (state == IDevice.State.off)
+                return;
+            switch (formatType)
+            {
+                case IDocument.FormatType.PDF:
+                    document = new PDFDocument($"PDFScan{ScanCounter}.pdf");
+                    ScanCounter++;
+                    Console.WriteLine($"{DateTime.Now} Scan: {document.GetFileName()}");
+                    break;
+
+                case IDocument.FormatType.JPG:
+                    document = new ImageDocument($"ImageScan{ScanCounter}.jpg");
+                    ScanCounter++;
+                    Console.WriteLine($"{DateTime.Now} Scan: {document.GetFileName()}");
+                    break;
+
+                case IDocument.FormatType.TXT:
+                    document = new TextDocument($"TextScan{ScanCounter}.txt");
+                    ScanCounter++;
+                    Console.WriteLine($"{DateTime.Now} Scan: {document.GetFileName()}");
+                    break;
+            }
+        }
+    }
+}
