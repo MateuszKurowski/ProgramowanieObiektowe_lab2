b31e66f06a7f977fb1d337c3753301c3e13eba48
Kamer <kuromateusz@gmail.com>
2022-03-24 23:31:09 +0100

    lab_3/Cwiczenie: Implementacja interfejsu - krok 1

---
+++ lab_3/Cwiczenie - Implementacja intefejsow/wellFormedTypes/Pracownik.cs
@@ -0,0 +1,79 @@
+using System;
+
+namespace wellFormedTypes
+{
+    class Pracownik : IEquatable<Pracownik>
+    {
+        private string _Nazwisko;
+       public string Nazwisko { 
+            get { return _Nazwisko; } 
+            set { _Nazwisko = value.Trim(); } 
+       }
+       public DateTime DataZatrudnienia { 
+            get { return _DataZatrudnienia; }
+            set{ 
+                if (value > DateTime.Now)
+                    throw new ArgumentException();
+                else _DataZatrudnienia = value;
+            } 
+        }
+     private DateTime _DataZatrudnienia;
+       public decimal Wynagrodzenie { 
+            get { return _Wynagrodzenie; } 
+            set { _Wynagrodzenie = (value < 0) ? 0 : value; } 
+       }
+       private decimal _Wynagrodzenie;
+
+       public int CzasZatrudnienia => (DateTime.Now - DataZatrudnienia).Days / 30;
+
+        public Pracownik()
+        {
+            Nazwisko = "Anonim";
+            DataZatrudnienia = DateTime.Now;
+            Wynagrodzenie = 0m;
+        }
+        public Pracownik(string nazwisko, DateTime dataZatrudnienia, decimal wynagrodzenie)
+        {
+            Nazwisko = nazwisko;
+            DataZatrudnienia = dataZatrudnienia;
+            Wynagrodzenie = wynagrodzenie;
+        }
+
+        public override string ToString()
+        {
+            return $"({Nazwisko}, {DataZatrudnienia:d MMM yyyy} ({CzasZatrudnienia}), {Wynagrodzenie} PLN)";
+        }
+
+        public bool Equals(Pracownik other)
+        {
+            if (other is null) return false;
+            if (Object.ReferenceEquals(this, other)) return true;
+            
+            return (Nazwisko == other.Nazwisko &&
+                    DataZatrudnienia == other.DataZatrudnienia &&
+                    Wynagrodzenie == other.Wynagrodzenie);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Pracownik)
+            {
+                return Equals((Pracownik)obj);
+            }
+            else return false;
+        }
+        
+        public override int GetHashCode() => (Nazwisko, DataZatrudnienia, Wynagrodzenie).GetHashCode();
+
+        public static bool Equals(Pracownik p1, Pracownik p2)
+        {
+            if ((p1 is null) && (p2 is null)) return true;
+            if ((p1 is null)) return false;
+
+            return p1.Equals(p2);
+        }
+
+        public static bool operator ==(Pracownik p1, Pracownik p2) => Equals(p1, p2);
+        public static bool operator !=(Pracownik p1, Pracownik p2) => !(p1 == p2);
+    }
+}
---
+++ lab_3/Cwiczenie - Implementacja intefejsow/wellFormedTypes/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace wellFormedTypes
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Krok1();
+    }
+
+    static void Krok1()
+    {
+        System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("--- Sprawdzenie poprawności tworzenia obiektu ---");
+        Pracownik p = new Pracownik("Kowalski", new DateTime(2010, 10, 1), 1_000);
+        Console.WriteLine(p);
+
+        Console.WriteLine("--- Sprawdzenie równości obiektów ---");
+        Pracownik p1 = new Pracownik("Nowak", new DateTime(2010, 10, 1), 1_000);
+        Pracownik p2 = new Pracownik("Nowak", new DateTime(2010, 10, 1), 1_000);
+        Pracownik p3 = new Pracownik("Kowalski", new DateTime(2010, 10, 1), 1_000);
+        Pracownik p4 = p1;
+        Console.WriteLine($"p1: {p1} hashCode: {p1.GetHashCode()}");
+        Console.WriteLine($"p2: {p2} hashCode: {p2.GetHashCode()}");
+        Console.WriteLine($"p3: {p3} hashCode: {p3.GetHashCode()}");
+        Console.WriteLine($"p4: {p4} hashCode: {p4.GetHashCode()}");
+        Console.WriteLine();
+
+        Console.WriteLine($"--- Równość dla p1 oraz p2 -");
+        Console.WriteLine($"Object.ReferenceEquals(p1, p2): {Object.ReferenceEquals(p1, p2)}");
+        Console.WriteLine($"p1.Equals(p2): {p1.Equals(p2)}");
+        Console.WriteLine($"p1 == p2: {p1 == p2}");
+        Console.WriteLine();
+
+        Console.WriteLine($"--- Równość dla p1 oraz p3 -");
+        Console.WriteLine($"Object.ReferenceEquals(p1, p3): {Object.ReferenceEquals(p1, p3)}");
+        Console.WriteLine($"p1.Equals(p3): {p1.Equals(p3)}");
+        Console.WriteLine($"p1 == p3: {p1 == p3}");
+        Console.WriteLine();
+
+        Console.WriteLine($"--- Równość dla p1 oraz p4 -");
+        Console.WriteLine($"Object.ReferenceEquals(p1, p4): {Object.ReferenceEquals(p1, p4)}");
+        Console.WriteLine($"p1.Equals(p3): {p1.Equals(p4)}");
+        Console.WriteLine($"p1 == p4: {p1 == p4}");
+    }
+}
+}
