84127b89f14b32d80377c5cedffc801ed13b053c
Mateusz Kurowski <kuromateusz@gmail.com>
2022-04-07 13:12:56 +0200

    lab - BitMatrix

---
+++ lab_5/BitMatrix/BitMatrix/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BitMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BitMatrix m = new BitMatrix(4);
+            //Console.WriteLine(m);
+            m[2, 3] = true;
+            //Console.WriteLine(m[2,3]);
+            //Console.WriteLine(m);
+
+            foreach (var x in m)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
