f1ab6b9e7862233d37c9e97434c71aec257f849b
Mateusz Kurowski <kuromateusz@gmail.com>
2022-05-19 16:16:21 +0200

    Framework do elementow tymczasowych, inicjalizacja zadania

---
+++ lab_5/BONUS-Framework_do_zarzadzania_elementami_tymczasowymi/Zarzadzanie_elementami_tymczasowymi/Zarzadzanie_elementami_tymczasowymi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zarzadzanie_elementami_tymczasowymi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
