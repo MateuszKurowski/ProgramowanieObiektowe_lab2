2826b7b456aec31caaa812385369887355e44880
Kamer <kuromateusz@gmail.com>
2022-06-08 22:50:39 +0200

    Egzamin - zadanie 5

--- Egzamin/Zadanie5/PhoneTests/PhoneTests/PhoneTests.cs
+++ Egzamin/Zadanie5/PhoneTests/PhoneTests/PhoneTests.cs
@@ -89,5 +89,72 @@ namespace PhoneTests
         {
             Phone p = new Phone("test", "123a56789");
         }
+
+        [TestMethod]
+        public void AddContact_good()
+        {
+            var number = "123456789";
+            var p = new Phone("Owner", "987654321");
+            for (int i = 0; i < 3; i++)
+            {
+                p.AddContact("test" + i, number);
+            }
+            Assert.AreEqual(3, p.Count);
+        }
+
+        [TestMethod]
+        public void AddContact_bad()
+        {
+            var number = "123456789";
+            var p = new Phone("Owner", "987654321");
+            p.AddContact("test", number);
+            var result = p.AddContact("test", number);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void AddContact_book_is_full()
+        {
+            var number = "123456789";
+            var p = new Phone("Owner", "987654321");
+            for (int i = 0; i < 100; i++)
+            {
+                p.AddContact("test" + i, number);
+            }
+            p.AddContact("test pelny", number);
+            Assert.AreEqual(3, p.Count);
+        }
+
+        [TestMethod]
+        public void Call_good()
+        {
+            var number = "123456789";
+            var name = "test";
+            var p = new Phone("Owner", "987654321");
+            p.AddContact(name, number);
+            var result = p.Call(name);
+            Assert.AreEqual($"Calling {number} ({name}) ...", result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Call_bad()
+        {
+            var number = "123456789";
+            var p = new Phone("Owner", "987654321");
+            p.AddContact("nietest", number);
+            var result = p.Call("test");
+            Assert.AreEqual("error", result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Call_bad2()
+        {
+            var p = new Phone("Owner", "987654321");
+            var result = p.Call("test");
+            Assert.AreEqual("error", result);
+        }
     }
 }
