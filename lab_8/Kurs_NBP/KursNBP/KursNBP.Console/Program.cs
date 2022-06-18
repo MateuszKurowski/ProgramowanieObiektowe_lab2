2928f804df0e0f60e4c9f1b719c7ab5b054a1980
Mateusz Kurowski <kuromateusz@gmail.com>
2022-06-18 19:40:50 +0200

    KursNBP

---
+++ lab_8/Kurs_NBP/KursNBP/KursNBP.Console/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+namespace KursNBP.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                CheckInput(args);
+            }
+            catch (Exception)
+            {
+                args = RepeatInput();
+            }
+
+            
+        }
+
+        private static void CheckInput(string[] args)
+        {
+            if (args == null || args.Length == 0)
+                throw new ArgumentNullException();
+
+            if (!AvailabeCurrency.Any(x => x == args[0].ToUpper()))
+                throw new ArgumentException("Not available or bad currency!");
+
+            if (!DateTime.TryParse(args[1], out var startDate))
+                throw new ArgumentException("Start date is in bad date format!");
+
+            if (!DateTime.TryParse(args[2], out var endDate))
+                throw new ArgumentException("End date is in bad date format!");
+
+            if (startDate > endDate)
+                throw new ArgumentException("End date cannot be later than start date!");
+
+            if (startDate > DateTime.Now || endDate > DateTime.Now)
+                throw new ArgumentOutOfRangeException("Unable to give a future date!");
+        }
+
+        private static string[] RepeatInput()
+        {
+            while(true)
+            {
+                System.Console.WriteLine("Bad input.");
+                System.Console.WriteLine("Please pass correct input (Example: EUR 2018-09-01 2018-09-20)");
+                try
+                {
+                    var input = System.Console.ReadLine();
+                    var args = input.Replace(",", " ").Replace(".", " ").Split(" ");
+
+                    CheckInput(args);
+
+                    return args;
+                }
+                catch (Exception)
+                {
+                    System.Console.Clear();
+                }
+            }
+            
+        }
+
+        public static readonly string[] AvailabeCurrency =
+        {
+            "USD",
+            "EUR",
+            "CHF",
+            "GBP"
+        };
+    }
+}
---
+++ lab_8/Kurs_NBP/KursNBP/KursNBP.Lib/NBPExchangeRate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace KursNBP.Lib
+{
+    public class NBPExchangeRate
+    {
+        public string Currency { get; init; }
+        public DateTime StartDate { get; init; }
+        public DateTime EndDate { get; init; }
+        private XDocument _xDocument;
+
+        public decimal AvverageExchangeRate { get; set; }
+        public double StandardDeviation { get; set; }
+        public decimal MinimumExchangeRate { get; set; }
+        public decimal MaximumExchangeRate { get; set; }
+
+        public NBPExchangeRate(string currency, DateTime startDate, DateTime endDate)
+        {
+            Currency = currency;
+            StartDate = startDate;
+            EndDate = endDate;
+
+            DownloadExchangeRate();
+
+            ProcessXml();
+        }
+
+        private void DownloadExchangeRate()
+        {
+
+        }
+
+        private void ProcessXml()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
